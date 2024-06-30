9520a0118832dbc6ccf212b5da944a5f48edf44a
nvaccarezza <nati.estudio@gmail.com>
2024-06-30 13:28:38 -0300

    Agregué rutas get de cuenta y usuarios

--- componentes/vistas_api.py
+++ componentes/vistas_api.py
@@ -8,7 +8,7 @@ from componentes.modelos import Sucursal
 
 from componentes.modelos import Cuenta
 
-
+#ruta de acceso para consultar sucursales
 @app.route("/api-proyecto/sucursales", methods=['GET'])
 def obtener_Sucursales():
     
@@ -17,5 +17,23 @@ def obtener_Sucursales():
     
     return jsonify(datos)
 
+# ruta de acceso para consultar cuentas
 
+@app.route("/api-proyecto/cuentas", methods=['GET'])
+def obtener_Cuentas():
+    
+    cuentas = Cuenta.obtener()
+    datos = [cuenta.__dict__ for cuenta in cuentas]
+    
+    return jsonify(datos)
+
+#rura de acceso para consultar usuarios
+
+@app.route("/api-proyecto/usuarios", methods=['GET'])
+def obtener_Usuarios():
+    
+    usuarios = Usuario.obtener()
+    datos = [usuario.__dict__ for usuario in usuarios]
+    
+    return jsonify(datos)
 

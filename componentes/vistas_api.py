379bfdbbbe95637132b7fea03e87390d8a08b06f
chino brian <bchinoyanarico@frba.utn.edu.ar>
2024-07-01 17:09:23 -0300

    progreso de vistas api -guardar usuario y modificarlo

--- componentes/vistas_api.py
+++ componentes/vistas_api.py
@@ -35,25 +35,58 @@ def crear_cuenta():
 def validar_cuenta():
     if request.method == 'POST':
         respuesta = {}
-        datos = request.json["datos"]
+        datos = request.get_json()
         print(f"Datos recibidos: {datos}")
 
         ingreso = Cuenta(datos['mail'],datos['clave'])
-        cuentas = Cuenta.obtener()  
-        cuentas = [cuenta.__dict__ for cuenta in cuentas]
+        cuenta = Cuenta.obtener('mail', datos['mail'])
+        if cuenta and ingreso.clave == cuenta.clave:
+            usuario = Usuario.obtener('id_cuenta', cuenta.id)
+
+            if not usuario:
+                respuesta['perfil'] = 0
+            else:
+                respuesta['perfil'] = 1
+            
+            respuesta['mensaje'] = 'Ingreso exitoso!'
+            respuesta['status'] = 200
+        else:
+            respuesta['mensaje'] = 'Verifique los datos enviados.'
+            respuesta['status'] = 409
+        #cuentas = Cuenta.obtener()  
+        #cuentas = [cuenta.__dict__ for cuenta in cuentas]
         
          
-        print(cuentas)
+        #print(cuentas)
         # if cuenta and ingreso.clave == cuenta.clave :
         #     usuario = Usuario('id_cuenta',cuenta.id)
         #     if not usuario:
-        #         respuesta['perfil'] = 0        
+        #    respuesta['perfil'] = 0        
         #     else:
         #         respuesta['perfil'] = 1
 
     return jsonify(respuesta) #devuelve un objeto
         
+@app.route("/api-proyecto/guardar-perfil", methods=['POST'])
+def crear_usuario():
     
+    datos = request.get_json()
+    print(f"Datos recibidos: {datos}")
+    cuenta = Cuenta.obtener('mail',datos['mail'])
+    nuevoUsuario = Usuario(cuenta.id,datos['nombre'],datos['apellido'],datos['dni'],datos['fecha_de_nacimiento'])
+    nuevoUsuario.guardar_db()
+        
+    
+    
+    return jsonify(datos)
+
+@app.route("/api-proyecto/modificar-perfil", methods=['POST'])
+def modificar_usuario():    
+    datos = request.get_json()
+    print(f"Datos recibidos: {datos}")
+    Usuario.modificar(datos)
+    return jsonify(datos)
+
     
     
 
